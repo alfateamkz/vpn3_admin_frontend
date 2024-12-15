7fc48cb21cee871f2e3b1329de2ee0162f9ba355
Alex <alexes.bochkarev@mail.ru>
2024-12-15 22:54:53 +0400

    [Add] user requests

--- src/shared/api/apiRequests.js
+++ src/shared/api/apiRequests.js
@@ -1,4 +1,4 @@
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 import { axiosInstance } from "./axiosInstance.js";
 
 export const apiRequests = {
@@ -9,19 +9,23 @@ export const apiRequests = {
   },
   stats: {
     orders: async (page, limit, type) => {
-      return axiosInstance.get(
-        `/stats/orders?page=${page}&limit=${limit}&type=${type}`,
-        {
-          headers: {
-            Authorization: `Bearer ${Cookies.get("accessToken")}`,
-          },
-        }
-      );
+      return axiosInstance.get("/stats/orders", {
+        params: {
+          page,
+          limit,
+          type,
+        },
+      });
     },
   },
   servers: {
     all: async (page, limit) => {
-      return axiosInstance.get(`/servers/all?page=${page}&limit=${limit}`);
+      return axiosInstance.get("/servers/all", {
+        params: {
+          page,
+          limit,
+        },
+      });
     },
     add: async (body) => {
       return axiosInstance.post("/servers/add", body);
@@ -35,7 +39,12 @@ export const apiRequests = {
   },
   subs: {
     all: async (page, limit) => {
-      return axiosInstance.get(`/subs/all?page=${page}&limit=${limit}`);
+      return axiosInstance.get("/subs/all", {
+        params: {
+          page,
+          limit,
+        },
+      });
     },
     add: async (body) => {
       return axiosInstance.post("/subs/add", body);
@@ -45,6 +54,33 @@ export const apiRequests = {
     },
     delete: async (server_id) => {
       return axiosInstance.delete(`/subs/delete/${server_id}`);
+    },
+  },
+  user: {
+    all: async (page, limit, search, referal_id = null) => {
+      const params = {
+        page,
+        limit,
+        search,
+      };
+
+      // Добавляем referal_id только если он указан
+      if (referal_id !== null) {
+        params.referal_id = referal_id;
+      }
+
+      return axiosInstance.get("/users/all", {
+        params,
+      });
+    },
+    one: async (user_id) => {
+      return axiosInstance.get(`/users/one/${user_id}`);
+    },
+    edit: async (body) => {
+      return axiosInstance.put(`/users/one/${body._id}`, body);
+    },
+    pushBalance: async (body) => {
+      return axiosInstance.post(`/users/push-balance/${body._id}`, body);
     },
   },
 };
