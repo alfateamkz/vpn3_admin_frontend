b8ead3a5412a6068bd2ec23c80ea0254cfdd99f3
Лев Низковский <2091levas@gmail.com>
2025-11-17 13:34:41 +0300

    update tilda mock3

--- src/components/payment/PaymentPage.jsx
+++ src/components/payment/PaymentPage.jsx
@@ -14,7 +14,6 @@ const PaymentPage = () => {
   const [referalCode, setReferalCode] = useState('');
   const [error, setError] = useState('');
   const [processing, setProcessing] = useState(false);
-  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     fetchSubs();
